1f908eb6af448412b71ba14a474bc522cdf3b39d
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2025-03-01 21:55:04 +0100

    feat: train step

--- src/classifier.py
+++ src/classifier.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 
 import numpy as np
@@ -67,14 +68,27 @@ class Classifier:
         self.J_D = J_D
 
         rng = np.random.default_rng() if rng is None else rng
-        self.layers = [initialize_state(N, rng) for _ in range(num_layers)]
+        self.initialize_state(rng)
+        self.initialize_couplings(rng)
+        self.activations = [np.sign for _ in range(self.num_layers)] + [
+            lambda x: int(x > 0)
+        ]
+
+    def initialize_state(self, rng: Optional[np.random.Generator] = None):
+        """Initializes the state of the network."""
+        rng = np.random.default_rng() if rng is None else rng
+        self.layers = [initialize_state(self.N, rng) for _ in range(self.num_layers)]
         self.layers.append(
-            initialize_readout_state(C, rng)
+            initialize_readout_state(self.C, rng)
         )  # num_layers + 1, N (layers[-1] is the readout layer)
+
+    def initialize_couplings(self, rng: Optional[np.random.Generator] = None):
+        """Initializes the couplings of the network."""
+        rng = np.random.default_rng() if rng is None else rng
         self.couplings = [
-            initialize_couplings(N, J_D, rng) for _ in range(num_layers)
+            initialize_couplings(self.N, self.J_D, rng) for _ in range(self.num_layers)
         ]  # num_layers, N, N
-        self.W = initialize_readout_couplings(N, C, rng)  # C, N
+        self.W = initialize_readout_couplings(self.N, self.C, rng)  # C, N
 
     def internal_field(self, layer_idx: int, neuron_idx: int):
         """Field due to interaction within each layer."""
@@ -118,12 +132,12 @@ class Classifier:
         y: Optional[np.ndarray] = None,
         ignore_right=False,
     ):
-        """Computes the local field perceived by a neuron.
-        :param layer_idx: layer index. Can pass y for the readout layer.
-        :param neuron_idx: neuron index.
-        :param x: input (left external field). If None, omit it.
-        :param y: one-hot target (right external field). If None, omit it.
-        :param ignore_right: if True, ignore the interaction from the right (next layer, or external field).
+        """Computes the local field perceived by a neuron. \\
+        :param layer_idx: layer index. Can pass y for the readout layer. \\
+        :param neuron_idx: neuron index. \\
+        :param x: input (left external field). If None, no left external field. \\
+        :param y: one-hot target (right external field). If None, no right external field. \\
+        :param ignore_right: if True, ignore the interaction from the right (next layer, or external field). \\
         :return: the local field perceived by the neuron.
         """
         internal_field = self.internal_field(layer_idx, neuron_idx)
@@ -131,28 +145,76 @@ class Classifier:
         right_field = 0 if ignore_right else self.right_field(layer_idx, neuron_idx, y)
         return internal_field + left_field + right_field
 
-    def relax(self, x: np.ndarray, y: np.ndarray):
-        """Relaxes the network to a stable state.
-        :param x: input (left external field). If None, omit it.
-        :param y: one-hot target (right external field). If None, omit it.
+    def relax(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        max_steps: int,
+        rng: Optional[np.random.Generator] = None,
+    ):
+        """Relaxes the network to a stable state. \\
+        :param x: input (left external field). If None, omit it. \\
+        :param y: one-hot target (right external field). If None, omit it. \\
+        :param max_steps: maximum number of sweeps over the network for updates. \\
+        :param rng: random number generator for update order. \\
+        :return: the number of full sweeps taken to converge (or max_steps if not converged).
         """
-        pass
+        step, made_update = 0, True
+        rng = np.random.default_rng() if rng is None else rng
+        while made_update and step < max_steps:
+            made_update = False
+            for layer_idx in range(self.num_layers + 1):
+                perm = rng.permutation(len(self.layers[layer_idx]))
+                for neuron_idx in perm:
+                    local_field = self.local_field(layer_idx, neuron_idx, x, y)
+                    update = self.activations[layer_idx](local_field)
+                    made_update = made_update or (
+                        update != self.layers[layer_idx][neuron_idx]
+                    )
+                    self.layers[layer_idx][neuron_idx] = update
+            step += 1
+        return step  # NOTE: if we converge during the (max_steps - 1)th sweep, we do not detect it
 
-    def apply_perceptron_rule(self, lr: float, threshold: float):
+    def apply_perceptron_rule(self, lr: float, threshold: float, x: np.ndarray):
         """Applies the perceptron learning rule to the network in its current state.
         :param lr: learning rate.
         :param threshold: stability threshold.
+        :param x: input.
         """
-        pass
+        for layer_idx in range(self.num_layers):
+            for neuron_idx in range(self.N):
+                local_field = self.local_field(
+                    layer_idx, neuron_idx, x=x, y=None, ignore_right=True
+                )
+                local_state = self.layers[layer_idx][neuron_idx]
+                if local_field * local_state > threshold:
+                    continue
+                self.couplings[layer_idx][neuron_idx, :] += (
+                    lr * local_state * self.layers[layer_idx][:]
+                )  # NOTE: this does not interfere with the local field computation in subsequent updates within the same sweep
+                self.couplings[layer_idx][neuron_idx, neuron_idx] = self.J_D
 
-    def train_step(self, x: np.ndarray, y: np.ndarray, lr: float, threshold: float):
+    def train_step(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        max_steps: int,
+        lr: float,
+        threshold: float,
+        rng: Optional[np.random.Generator] = None,
+    ):
         """Performs a single training step.
         :param x: input.
         :param y: one-hot target.
         :param lr: learning rate.
         :param threshold: stability threshold.
         """
-        pass
+        rng = np.random.default_rng() if rng is None else rng
+        self.initialize_state(rng)
+        steps = self.relax(x, y, max_steps, rng)
+        if steps == max_steps:
+            logging.warning(f"Did not detect convergence in {max_steps} full sweeps.")
+        self.apply_perceptron_rule(lr, threshold, x)
 
     def inference(self, x: np.ndarray, repeat: int = 1):
         """Performs inference with the network.
