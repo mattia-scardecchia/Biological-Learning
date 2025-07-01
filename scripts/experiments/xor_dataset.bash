bafd84608a98738993ee97ec0b76a562fc1f8a8b
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2025-07-01 14:21:36 +0200

    chore: config

--- scripts/experiments/xor_dataset.bash
+++ scripts/experiments/xor_dataset.bash
@@ -28,13 +28,13 @@ conda run -p /home/3144860/.conda/envs/bio python scripts/train.py -cn xor_datas
 conda run -p /home/3144860/.conda/envs/bio python scripts/mlp_train.py -cn xor_mlp --multirun \
     name=xor/perceptron \
     'model.hidden_dims=[]' \
-    num_epochs=10 \
+    trainer.max_epochs=10 \
     seed=0,1,2
 
 # mlp
 conda run -p /home/3144860/.conda/envs/bio python scripts/mlp_train.py -cn xor_mlp --multirun \
     name=xor/mlp \
     'model.hidden_dims=[100]' \
-    num_epochs=10 \
+    trainer.max_epochs=10 \
     model.activation=square_tanh,beta_tanh \
     seed=0,1,2
