451b1be0ce05d543f6f2f4c33e44b7b914a06168
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2025-07-07 14:53:22 +0200

    chore: check tuning JD=0

---
+++ scripts/experiments/check_tuning_JD0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda run -p /home/3144860/.conda/envs/bio python scripts/grid_search.py -cn baseline_1layer_largeP \
+    name=tuning-JD0 \
+    J_D=0.0 \
+    lambda_input_skip=5.0 \
+    double_dynamics=true \
+    '+threshold_hidden_values=[0.0,0.3,0.6,0.9,1.2]' \
+    '+lr_J_values=[0.001,0.005,0.01,0.02,0.03,0.05]' \
+    '+lambda_wback_values=[0.3,0.6,0.9]' \
+    num_epochs=20
